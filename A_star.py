f5b76891b50f8478c405fd20183d9e39cae8f677
Shreenath013 <137416704+Shreenath013@users.noreply.github.com>
2025-05-04 19:47:27 +0530

    Add files via upload

--- A_star.py
+++ A_star.py
@@ -1,93 +1,146 @@
 import heapq
 
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-----")
 
-def check_winner(board, player):
-    for row in board:
-        if all(cell == player for cell in row):
-            return True
-    for col in range(3):
-        if all(board[row][col] == player for row in range(3)):
-            return True
-    if all(board[i][i] == player for i in range(3)):
-        return True
-    if all(board[i][2 - i] == player for i in range(3)):
-        return True
-    return False
+board = [['_', '_', '_'],
+         ['_', '_', '_'],
+         ['_', '_', '_']]
 
-def is_full(board):
-    return all(cell != " " for row in board for cell in row)
 
-def possible_moves(board):
-    return [(i, j) for i in range(3) for j in range(3) if board[i][j] == " "]
+def printboard():
+    for i in board:
+        print(i)
+    print("\n\n")
 
-def heuristic_score(board, symbol):
-    opponent = 'X' if symbol == 'O' else 'O'
-    lines = board + \
-            [[board[i][j] for i in range(3)] for j in range(3)] + \
-            [[board[i][i] for i in range(3)], [board[i][2 - i] for i in range(3)]]
-    score = 0
-    for line in lines:
+
+def isBoardfull():
+    for i in board:
+        for j in i:
+            if j == '_':
+                return False
+    return True
+
+
+def heuristic_score(symbol):
+    opponent = 'O' if symbol == 'X' else 'X'
+
+
+    def line_score(line):
         if line.count(symbol) == 3:
-            score += 100
-        elif line.count(symbol) == 2 and line.count(" ") == 1:
-            score += 10
-        elif line.count(opponent) == 2 and line.count(" ") == 1:
-            score -= 10
+            return 100
+        elif line.count(symbol) == 2 and line.count('_') == 1:
+            return 10  
+        elif line.count(opponent) == 2 and line.count('_') == 1:
+            return -10  
+        return 0
+
+
+    score = 0
+
+
+    for row in board:
+        score += line_score(row)
+
+
+    for col in range(3):
+        column = [board[row][col] for row in range(3)]
+        score += line_score(column)
+
+
+    main_diag = [board[i][i] for i in range(3)]
+    anti_diag = [board[i][2 - i] for i in range(3)]
+    score += line_score(main_diag)
+    score += line_score(anti_diag)
+
+
     return score
 
-def a_star(board, symbol):
-    frontier = [(0, board, 0, None)]
-    visited = set()
 
-    while frontier:
-        f_score, current, steps, last_move = heapq.heappop(frontier)
-        if check_winner(current, symbol):
-            return last_move
-        for i, j in possible_moves(current):
-            new_board = [row[:] for row in current]
-            new_board[i][j] = symbol
-            board_key = tuple(map(tuple, new_board))
-            if board_key in visited:
-                continue
-            visited.add(board_key)
-            g_cost = steps + 1
-            h = heuristic_score(new_board, symbol)
-            f_score = g_cost + h
-            heapq.heappush(frontier, (f_score, new_board, g_cost, (i, j)))
-    return None
+def get_available_moves():
+    return [(i, j) for i in range(3) for j in range(3) if board[i][j] == '_']
 
-def main():
-    board = [[" "] * 3 for _ in range(3)]
-    while True:
-        print_board(board)
-        row, col = map(int, input("Your turn (Enter row col): ").split())
-        if board[row][col] != " ":
-            print("Invalid move! Try again.")
-            continue
-        board[row][col] = "X"
-        if check_winner(board, "X"):
-            print_board(board)
-            print("You win!")
-            break
-        if is_full(board): 
-            print_board(board)
-            print("Draw!")
-            break
-        ai_move = a_star(board, "O")
-        if ai_move:
-            ai_row, ai_col = ai_move
-            board[ai_row][ai_col] = "O"
-            if check_winner(board, "O"):
-                print_board(board)
-            print("AI wins!")
-            break
-            if is_full(board): print_board(board)
-            print("Draw!")
-            break
 
-if __name__ == "__main__":
-    main()
+def Computer_turn():
+    if isBoardfull():
+        print("Match Draw....")
+        return
+
+
+    def calculate_f_score(move, depth):
+        i, j = move
+        board[i][j] = 'X'
+        g = depth  
+        h = heuristic_score('X')  
+        board[i][j] = '_'  
+        return g + h
+
+
+    priority_queue = []
+    depth = 0
+
+
+    for move in get_available_moves():
+        f_score = calculate_f_score(move, depth)
+        heapq.heappush(priority_queue, (-f_score, move))  
+
+
+    _, best_move = heapq.heappop(priority_queue)
+    i, j = best_move
+    board[i][j] = 'X'
+
+
+    if win_by_symbol('X'):
+        print("Computer Won.......!!!")
+        printboard()
+        exit(0)
+
+
+    printboard()
+
+
+def Your_turn():
+    if isBoardfull():
+        print("Match Draw....")
+        return
+
+
+    print("\nYour turn Now:- ")
+    i = int(input("Enter i (0-2): "))
+    j = int(input("Enter j (0-2): "))
+
+
+    if board[i][j] == '_':
+        board[i][j] = 'O'
+        if win_by_symbol('O'):
+            print("You Won.......!!!")
+            printboard()
+            exit(0)
+    else:
+        print("Invalid move. Try again.")
+        Your_turn()
+        return
+
+    printboard()
+
+
+def win_by_symbol(symbol):
+    for i in range(3):
+        if win_by_row(symbol, i) or win_by_col(symbol, i):
+            return True
+    return win_by_diagonal(symbol)
+
+
+def win_by_diagonal(symbol):
+    return all(board[i][i] == symbol for i in range(3)) or all(board[i][2 - i] == symbol for i in range(3))
+
+
+def win_by_row(symbol, i):
+    return all(board[i][j] == symbol for j in range(3))
+
+
+def win_by_col(symbol, j):
+    return all(board[i][j] == symbol for i in range(3))
+
+
+while not isBoardfull():
+    Your_turn()
+    Computer_turn()
